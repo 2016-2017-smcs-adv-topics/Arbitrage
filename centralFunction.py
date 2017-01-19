57d0029c6fbec632b9dbd97ed58dacaffad1e957
Joseph Smith <josephsmith@stmarksschool.org>
2017-01-19 14:09:51 -0500

    Setting up Functions
    preliminary function code

---
+++ centralFunction.py
@@ -0,0 +1,18 @@
+
+def determineMarketPrice (priceData):
+
+    marketSummary = []
+
+    return marketSummary
+
+def potentialRiskCoefficient (marketSummary, sellerRating):
+
+    riskCoefficient = float
+
+    return riskCoefficient
+
+def potentialProfitability (marketSummary, riskCoefficient):
+
+    profitabilityCoefficient = float
+
+    return profitabilityCoefficient

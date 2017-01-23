795753d2065ad1a71e2a2b89e47c5bbc052a004b
Joseph Smith <josephsmith@stmarksschool.org>
2017-01-23 09:46:55 -0500

    working towards a running mathematical model

--- centralFunction.py
+++ centralFunction.py
@@ -1,6 +1,39 @@
 
+from collections import defaultdict
+
+class arbObject:
+
+    def __init__(self):
+        determineMarketPrice()
+        potentialRisk()
+        potentialProfitability()
+        rudolfElasticity()
+        marketEffervescence()
+        self.finalCoefficient = bullCoefficient()
+
+    def determinePurchase(self, finalCoefficient):
+        self.finalCoefficient
+
+
+def shellSortPrice (priceData):
+
+    #The following finds indecies of dupes in a list (need to decipher)
+    D = defaultdict(list)
+    for i, item in enumerate(priceData):
+        D[item].append(i)
+        D = {k: v for k, v in D.items() if len(v) > 1}
+    #weight items with more duplicate prices
+
+    # shell sort the weighted/deduped list
+
 def determineMarketPrice (priceData):
-    marketSummary = []
+
+    marketSummary = quickSortPrice(priceData)  #determine 1st-99th percentiles
+    twoFive = []
+
+    for x in range(0,24):
+        twoFive.append(marketSummary[x])
+
     return marketSummary
 
 def potentialRisk (marketSummary, sellerRating):

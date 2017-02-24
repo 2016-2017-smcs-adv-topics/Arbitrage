116dc2e9e9c2b34ada9e995f29e5ece208b1403e
Seth Battis <seth@battis.net>
2017-02-24 09:11:24 -0500

    BeautifulSoup (in theory)

--- webCrawler.py
+++ webCrawler.py
@@ -1,23 +1,21 @@
 import scrapy
+import BeautifulSoup
 
 class Arbitrage(scrapy.Spider):
     name = 'arbi'
     start_urls = ["http://www.ebay.com/sch/i.html?_from=R40&_nkw=CALLAWAY%20GOLF%202015%20GREAT%20BIG%20BERTHA%20DRIVER&rt=nc&LH_ItemCondition=4&_udlo&_udhi&LH_PrefLoc=1&LH_AllListings=1"]
 
     def parse(self, response):
-        SET_SELECTOR = '.set'
-        for set in response.css(SET_SELECTOR):
+        soup = BeautifulSoup(response, 'html.parser')
+        for item in soup.select('#ListViewInner li'):
 
-            NAME_SELECTOR = './/ul[li]/a/text()'
-            PRICE_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
-            
             yield {
-                'name': set.css(NAME_SELECTOR).extract_first(),
-                'pieces': set.xpath(PRICE_SELECTOR).extract_first(),
+                'name': item.find_next("h3", class_="lvtitle"),
+                'price': item.find_next("li", class_="lvprice prc")
             }
 
-        NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
-        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+        NEXT_PAGE_SELECTOR = '.gspr.next a ::attr(href)'
+        next_page = response.select(NEXT_PAGE_SELECTOR)
         if next_page:
             yield scrapy.Request(
                 response.urljoin(next_page),

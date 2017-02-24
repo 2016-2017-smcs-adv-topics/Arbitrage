6f1065b5c3717fdc76b1d57ec25ac55d451ae6d7
Joseph Smith <josephsmith@stmarksschool.org>
2017-02-24 08:50:12 -0500

    Stuff

--- webCrawler.py
+++ webCrawler.py
@@ -1,22 +1,19 @@
 import scrapy
 
-class BrickSetSpider(scrapy.Spider):
-    name = 'brick_spider'
+class Arbitrage(scrapy.Spider):
+    name = 'arbi'
     start_urls = ["http://www.ebay.com/sch/i.html?_from=R40&_nkw=CALLAWAY%20GOLF%202015%20GREAT%20BIG%20BERTHA%20DRIVER&rt=nc&LH_ItemCondition=4&_udlo&_udhi&LH_PrefLoc=1&LH_AllListings=1"]
 
     def parse(self, response):
         SET_SELECTOR = '.set'
         for set in response.css(SET_SELECTOR):
 
-            NAME_SELECTOR = 'h3 class = "lvtitle"'
+            NAME_SELECTOR = './/ul[li]/a/text()'
             PRICE_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
-            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
-            IMAGE_SELECTOR = 'img ::attr(src)'
+            
             yield {
                 'name': set.css(NAME_SELECTOR).extract_first(),
                 'pieces': set.xpath(PRICE_SELECTOR).extract_first(),
-                'minifigs': set.xpath(MINIFIGS_SELECTOR).extract_first(),
-                'image': set.css(IMAGE_SELECTOR).extract_first(),
             }
 
         NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
